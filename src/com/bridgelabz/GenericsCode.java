a8666e4683fd7df9240d6446d0bdc9778449b902
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-21 22:14:31 +0530

    Update GenericsCode.java

--- src/com/bridgelabz/GenericsCode.java
+++ src/com/bridgelabz/GenericsCode.java
@@ -1,7 +1,28 @@
 package com.bridgelabz;
 
 public class GenericsCode {
-    public static void main(String[] args) {
+//       Integer Element
+    public void FindMaxInt(Integer a, Integer b, Integer c){
+        int max = a;
+        if((b.compareTo(max))>0){
+            max = b;
+        }
+        if((c.compareTo(max))>0){
+            max =c;
+        }
+        System.out.println("Maximum of " + a + "," + b + "," + c + " is: " + max);
+    }
 
+    public static void main(String[] args) {
+        GenericsCode max = new GenericsCode();
+        Integer a=30, b=20, c=10;
+        System.out.println("Integer Values....");
+//        test case 1 :- Given max Number at 1st Position return the same Number
+        max.FindMaxInt(a, b, c);
+//        test case 2 :- Given max Number at 2nd Position return the same Number
+        max.FindMaxInt(b, a, c);
+//        test case 3 :- Given max Number at 3rd Position return the same Number
+        max.FindMaxInt(c, b, a);
     }
 }
+
